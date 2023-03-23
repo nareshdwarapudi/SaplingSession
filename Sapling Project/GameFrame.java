9f2a37d90c0dd020fd38e09f2830051cd6f6df89
nareshdwarapudi <118378234+nareshdwarapudi@users.noreply.github.com>
2023-03-23 22:04:10 +0530

    Add files via upload

---
+++ Sapling Project/GameFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+public class GameFrame {
+    JFrame frame;
+    GameFrame()
+    {
+        frame=new JFrame("Pong Game");
+        frame.setLayout(null);
+        //Creating panel
+        Table table=new Table();
+        table.setBounds(100,100,800,200);
+        table.setBackground(Color.black);
+        //Adding the table to my Frame
+        frame.add(table);
+
+        frame.setBounds(200,200,1000,555);
+        frame.getContentPane().setBackground(Color.black);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+    }
+}

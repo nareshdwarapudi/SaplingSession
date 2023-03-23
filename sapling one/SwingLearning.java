2f0b0b9a2ddeed33c07af1b6e78c5451f251384a
nareshdwarapudi <118378234+nareshdwarapudi@users.noreply.github.com>
2023-03-23 16:54:52 +0530

    Add files via upload

---
+++ sapling one/SwingLearning.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SwingLearning {
+
+     public static void main(String[] args){
+         JFrame frame=new JFrame(title: "Sapling Session");
+         frame.setBounds(x:20,y:200,width:500,height:500);
+         frame.getConventPane().setbackground(Color.green);
+         frame.getVisible(true);
+     }
+
+}
